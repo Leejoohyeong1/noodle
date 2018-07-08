e8e0664fdce721620511b49114ecc28d98f0a914
lee <tkdrb4807@gmail.com>
2018-07-08 22:38:09 +0900

    json create

--- index.js
+++ index.js
@@ -4,23 +4,22 @@ var request = require('request');
 var app = express();
 
 app.get('/', function (req, res) {
+  var array = new Array();
   request('http://www.samyangfoods.com/kor/brand/ajaxProductList.do?pageIndex=1&pageUnit=1000&searchCateCd1=45&searchCateCd2=49&searchNewUseYn=&seq=&_=1527087084424', function (error, response, body) {
     var $ = cheerio.load(body);
-    $('#productList > ul > li').each(function(i, elem) {
-      console.log("---------------------------------------------------------"); 
-        var url =  $(elem).find('a').attr('onclick'); 
-        url = url.replace(/[^0-9]/g,'');
-
-
     
-        console.log(url);
-
-        console.log($(elem).find('img').attr('src'));
-        console.log($(elem).find('div').eq(1).text());
-      console.log("---------------------------------------------------------"); 
+    $('#productList > ul > li').each(function(i, elem) {
+      var object = new Object();
+      object.NoodlePk = $(elem).find('a').attr('onclick').replace(/[^0-9]/g,'');
+      object.ImageUrl = $(elem).find('img').attr('src');
+      object.Name = $(elem).find('div').eq(1).text(); 
+      console.log(object);
+      array.push(object);
     });
+    res.json(array);
   });
 //http://www.samyangfoods.com/kor/brand/view.do?seq=412
+  
 });
 
 app.listen(3000, function () {
